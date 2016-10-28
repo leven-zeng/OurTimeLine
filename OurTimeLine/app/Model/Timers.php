65e865670507a2a4e4cb0db647d69650334ae01b
Leven <leven.z@xuea100.com>
2016-10-28 09:36:29 +0800

    提交初始项目

---
+++ OurTimeLine/app/Model/Timers.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Timers extends Model
+{
+    //
+    protected $fillable = [
+        'year', 'date', 'address','title', 'content', 'authorId',
+    ];
+
+
+}
---
+++ OurTimeLine/app/Model/images.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class images extends Model
+{
+    //
+    protected $fillable = [
+        'timerId', 'imgname'
+    ];
+}
